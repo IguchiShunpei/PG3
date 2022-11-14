65599f6d52a649b829a09c51a3dc28d6fe4c9e27
IguchiShunpei <k021g1129@g.neec.ac.jp>
2022-11-14 09:14:42 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,35 +1,48 @@
 #include<stdio.h>
+#include "math.h"
 
-template<typename Type>
-//小さいほうの値を返す
+//再帰的な賃金
+int RecoursiveSalary(int hour) {
 
-Type Min(Type a, Type b)
-{
-	if (a <= b)
-	{
-		return static_cast<Type>(a);
+	if (hour == 1) {
+		return (100);
 	}
-	else if (a > b)
+	return RecoursiveSalary(hour - 1) * 2 - 50;
+
+}
+
+//賃金を比較する関数
+void comparison(int flat, int rec)
+{
+	//一般時給と再帰的な時給を比較する
+	int n = flat - rec;
+	//絶対値
+	n = abs(n);
+	if (flat > rec)
 	{
-		return static_cast<Type>(b);
+		printf("一般のほうが%d円高い。\n\n", n);
+	}
+	else
+	{
+		printf("再帰的のほうが%d円高い。\n\n", n);
 	}
 }
 
-template<>
-char Min(char a, char b)
-{
-	return 0;
-}
+int main() {
+	//一般的な賃金
+	int price = 0;
+	int hour = 1;
+	int result = 0;
 
-int main(void)
-{
-	printf("%d\n", Min<int>(1, 2));
-	printf("%f\n", Min<float>(1.5f, 2.5f));
-	printf("%lf\n", Min<double>(1.5, 2.5));
-	if (Min<char>('a', 'b') == 0)
-	{
-		printf("数字は代入できません");
+
+	for (int i = 1; i < 10; i++) {
+		price += 1072;
+		printf("%d時間で%d円\n", i, price);
+
+		result += RecoursiveSalary(i);
+		printf("%d時間で%d円\n\n", i, result);
+
+		comparison(price, result);
 	}
-
 	return 0;
 }
