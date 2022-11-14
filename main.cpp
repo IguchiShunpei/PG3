b0d59a9f019763abc227aa67dcd67e835498b64d
IguchiShunpei <k021g1129@g.neec.ac.jp>
2022-11-14 09:12:42 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include<stdio.h>
 
-int main()
+template<typename Type>
+//¬‚³‚¢‚Ù‚¤‚Ì’l‚ğ•Ô‚·
+
+Type Min(Type a, Type b)
 {
+	if (a <= b)
+	{
+		return static_cast<Type>(a);
+	}
+	else if (a > b)
+	{
+		return static_cast<Type>(b);
+	}
+}
+
+template<>
+char Min(char a, char b)
+{
+	return 0;
+}
+
+int main(void)
+{
+	printf("%d\n", Min<int>(1, 2));
+	printf("%f\n", Min<float>(1.5f, 2.5f));
+	printf("%lf\n", Min<double>(1.5, 2.5));
+	if (Min<char>('a', 'b') == 0)
+	{
+		printf("”š‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+	}
 
 	return 0;
 }
