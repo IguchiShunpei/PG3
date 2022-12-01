f5d2a28e1dc62a4e74505b468fe36e97be4b741a
IguchiShunpei <k021g1129@g.neec.ac.jp>
2022-12-01 23:49:32 +0900

    02_01

--- main.cpp
+++ main.cpp
@@ -1,50 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <time.h>
-#include <functional>
+#include <list>
+#include <iostream>
 
-typedef void (*PFunc)(int);
+using namespace std;
 
-void DiceResult(int number) {
-	srand(time(nullptr));
-	int dice = rand() % 6 + 1;
-
-	if (dice % 2 == number) {
-		printf("当たり\n");
+int main() {
+	list<const char*> Yamanote = { "Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
+									  "Uguisudani","Nippori","Tabata","Komagome",
+									  "Sugamo","Otsuka","Ikebukuro","Mejiro" ,"Takadanobaba",
+									  "Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+									  "Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
+									  "Tamachi","Hamamatsucho","Shimbashi","Yurakucho\n" };
+	// 出力
+	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		cout << *itr << endl;
 	}
-	else {
-		printf("はずれ\n");
+	//西日暮里を挿入
+	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		if (*itr == "Tabata") {
+			itr = Yamanote.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
 	}
-	printf("サイコロの出目は%dでした\n", dice);
-};
+	// 出力
+	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		cout << *itr << endl;
+	}
 
-int main(void) {
-
-	PFunc p;
-
-	int number = 0;
-	int second = 3;
-
-	printf("半(奇数)なら[1],丁(偶数)なら[0]を入力してください\n");
-
-	scanf_s("%d", &number);
-
-	std::function<void(PFunc, int)>SetTimeout = [=](PFunc p, int second) {
-		printf("結果...");
-
-		//コールバック関数を呼び出す
-		Sleep(second * 1000);
-
-		//macやUnix系OSの場合
-		//sleep(second);
-		p(number);
-	};
-
-	p = DiceResult;
-
-	SetTimeout(p, second);
-
+	// 高輪ゲートウェイを挿入
+	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			itr = Yamanote.insert(itr, "Takanawa Gatewan");
+			++itr;
+		}
+	}
+	// 出力
+	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		cout << *itr << endl;
+	}
 	return 0;
 
 }
