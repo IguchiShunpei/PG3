d7b1509e12554703e6843bdb6f6a4dd2c2d50ebe
IguchiShunpei <k021g1129@g.neec.ac.jp>
2022-12-05 10:33:30 +0900

    駅名の修正と年代の追加

--- main.cpp
+++ main.cpp
@@ -5,37 +5,50 @@
 using namespace std;
 
 int main() {
-	list<const char*> Yamanote = { "Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
-									  "Uguisudani","Nippori","Tabata","Komagome",
-									  "Sugamo","Otsuka","Ikebukuro","Mejiro" ,"Takadanobaba",
-									  "Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
-									  "Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
-									  "Tamachi","Hamamatsucho","Shimbashi","Yurakucho\n" };
+	//駅名
+	list<const char*> YamanoteLine =
+	{ "Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
+	  "Uguisudani","Nippori","Tabata","Komagome",
+	  "Sugamo","Otsuka","Ikebukuro","Mejiro" ,"Takadanobaba",
+	  "Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+	  "Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
+	  "Tamachi","Hamamatsucho","Shimbashi","Yurakucho\n--------\n"
+	};
 	// 出力
-	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+	cout << "-1970年-\n";
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
+	{
 		cout << *itr << endl;
 	}
 	//西日暮里を挿入
-	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
-		if (*itr == "Tabata") {
-			itr = Yamanote.insert(itr, "Nishi-Nippori");
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = YamanoteLine.insert(itr, "Nishi-Nippori");
 			++itr;
 		}
 	}
 	// 出力
-	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+	cout << "-2019年-\n";
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
+	{
 		cout << *itr << endl;
 	}
 
 	// 高輪ゲートウェイを挿入
-	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
-		if (*itr == "Tamachi") {
-			itr = Yamanote.insert(itr, "Takanawa Gatewan");
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
+	{
+		if (*itr == "Tamachi")
+		{
+			itr = YamanoteLine.insert(itr, "Takanawa Gateway");
 			++itr;
 		}
 	}
 	// 出力
-	for (list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+	cout << "-2022年-\n";
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
+	{
 		cout << *itr << endl;
 	}
 	return 0;
