bf18c7fbf79107dc67d6eb98c7b598174c847164
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-01-25 13:01:52 +0900

    03_01.シングルトンパターン

--- main.cpp
+++ main.cpp
@@ -1,56 +1,12 @@
 #include <stdio.h>
-#include <list>
-#include <iostream>
+#include "SceneManager.h"
 
-using namespace std;
+int sceneNo = 0;
 
-int main() {
-	//駅名
-	list<const char*> YamanoteLine =
-	{ "Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
-	  "Uguisudani","Nippori","Tabata","Komagome",
-	  "Sugamo","Otsuka","Ikebukuro","Mejiro" ,"Takadanobaba",
-	  "Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
-	  "Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
-	  "Tamachi","Hamamatsucho","Shimbashi","Yurakucho\n--------\n"
-	};
-	// 出力
-	cout << "-1970年-\n";
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
-	{
-		cout << *itr << endl;
-	}
-	//西日暮里を挿入(itaratorを移動させてinsert()を使う)
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
-	{
-		if (*itr == "Tabata")
-		{
-			itr = YamanoteLine.insert(itr, "Nishi-Nippori");
-			++itr;
-		}
-	}
-	// 出力
-	cout << "-2019年-\n";
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
-	{
-		cout << *itr << endl;
-	}
-
-	// 高輪ゲートウェイを挿入
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
-	{
-		if (*itr == "Tamachi")
-		{
-			itr = YamanoteLine.insert(itr, "Takanawa Gateway");
-			++itr;
-		}
-	}
-	// 出力
-	cout << "-2022年-\n";
-	for (list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr)
-	{
-		cout << *itr << endl;
-	}
+int main() 
+{
+	//インスタンスを取得して、すぐ使用
+	SceneManager::GetInstance()->ChangeScene(sceneNo);
+	
 	return 0;
-
 }
