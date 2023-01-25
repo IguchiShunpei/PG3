50f883924bb6d6f054b48fa20b1c6cb9a82b7c1c
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-01-25 14:49:10 +0900

    03_03.メンバ関数ポインタ

--- main.cpp
+++ main.cpp
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include "Enemy.h"
 #include <windows.h>
-#include <iostream>
-using namespace std;
-
-bool Enemy::isEnemyAlive;
 
 int main()
 {
-	Enemy* enemy1 = new Enemy;
-	Enemy* enemy2 = new Enemy;
-	Enemy* enemy3 = new Enemy;
+	Enemy* enemy = new Enemy;
 
-	cout << Enemy::isEnemyAlive << endl;
-	cout << Enemy::isEnemyAlive << endl;
-	cout << Enemy::isEnemyAlive << endl;
-
-	Sleep(3 * 1000);
-
-	delete enemy1;
-	cout << Enemy::isEnemyAlive << endl;
-	cout << Enemy::isEnemyAlive << endl;
-	cout << Enemy::isEnemyAlive << endl;
+	while (true)
+	{
+		enemy->Update();
+		Sleep(2 * 1000);
+	}
 
 	return 0;
 }
